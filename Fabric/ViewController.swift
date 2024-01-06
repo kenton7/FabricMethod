f21c69186055a5bc207dfcc29e25ef7c66014c64
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2024-01-06 15:48:13 +0300

    Initial Commit

---
+++ Fabric/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fabric
+//
+//  Created by Илья Кузнецов on 06.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
